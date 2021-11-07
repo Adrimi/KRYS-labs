2c902cc1829954c4fa18f186f1f2a9e7141f0625
Adrian Szymanowski <adriano100417@gmail.com>
2021-11-07 20:42:56 +0100

    General - modular arithmetic 1

---
+++ General/general_modular_arithmetic_1.py
@@ -0,0 +1,12 @@
+a = 11
+m = 6
+
+# parafrazując - ile razy "obróciło licznik"
+# https://realpython.com/python-modulo-operator/#modulo-operator-and-divmod
+print(divmod(a, m))
+print(a % m)
+
+c = 8146798528947
+d = 17
+print(divmod(c, d))
+print(c % d)
