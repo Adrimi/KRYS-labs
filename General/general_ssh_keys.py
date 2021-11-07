e59b7f0b2084a83acbf2f6b5cb3a750948f05195
Adrian Szymanowski <adriano100417@gmail.com>
2021-11-07 21:16:14 +0100

    General - SSH Keys

---
+++ General/general_ssh_keys.py
@@ -0,0 +1,10 @@
+from Crypto.PublicKey import RSA
+import subprocess
+# from ssl import 
+
+filepath = 'General/bruce_rsa.pub'
+result = subprocess.run(['ssh-keygen', '-f' , filepath, '-e', '-m', 'pem'], stdout=subprocess.PIPE)
+
+modulo = RSA.importKey(result.stdout).n
+
+print(modulo)
