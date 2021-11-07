8060cf14dba94adec3290e8d8e5141eac037753f
Adrian Szymanowski <adriano100417@gmail.com>
2021-11-07 22:34:37 +0100

    cleanup

--- General/general_ssh_keys.py
+++ General/general_ssh_keys.py
@@ -1,6 +1,5 @@
 from Crypto.PublicKey import RSA
 import subprocess
-# from ssl import 
 
 filepath = 'General/bruce_rsa.pub'
 result = subprocess.run(['ssh-keygen', '-f' , filepath, '-e', '-m', 'pem'], stdout=subprocess.PIPE)
