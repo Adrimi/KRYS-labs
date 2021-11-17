9b081d7338ffe78ffb4707f404cffc2f1db53962
Adrian Szymanowski <adriano100417@gmail.com>
2021-11-17 21:17:59 +0100

    Mathematics - Quadratic Residues

---
+++ Mathematics/quadratic_residues.py
@@ -0,0 +1,30 @@
+from math import sqrt
+
+
+def main():
+    p = 29
+    ints = [14, 6, 11]
+
+    quad_res = quadratic_residues(p)
+
+    print(quad_res)
+
+    for int in ints:
+        for square_root, residue in quad_res:
+            if int is residue:
+                print(f'Answer is {square_root}')
+
+
+def quadratic_residues(p):
+    list = []
+
+    for x in range(1, p-1):
+        residue = x*x % p
+        if not residue in [x[1] for x in list]:
+            list.append((x, residue))
+
+    return list
+
+
+if __name__ == '__main__':
+    main()
