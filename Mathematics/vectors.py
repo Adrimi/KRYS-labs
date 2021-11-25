4dc8a7849408bda4975559df9688115b94323813
Adrian Szymanowski <adriano100417@gmail.com>
2021-11-25 09:44:17 +0100

    Mathematics - Vectors

---
+++ Mathematics/vectors.py
@@ -0,0 +1,17 @@
+from vector import obj
+
+# TASK: Calculate 3*(2*v - w) ∙ 2*u
+
+
+def main():
+    v = obj(x=2, y=6, z=3)
+    w = obj(x=1, y=0, z=0)
+    u = obj(x=7, y=7, z=2)
+
+    result = (3 * (2 * v - w)) @ (2 * u)
+
+    print(f'Solution is {result}')
+
+
+if __name__ == '__main__':
+    main()
