5d16013d637500c209d2e7c59cb2763caf054d27
Adrian Szymanowski <adriano100417@gmail.com>
2021-11-25 22:50:32 +0100

    Mathematics - size and basis

---
+++ Mathematics/size-and-basis.py
@@ -0,0 +1,14 @@
+from math import sqrt
+from functools import reduce
+
+
+def main():
+    v = [4, 6, 2, 5]
+
+    size = sqrt(reduce(lambda a, b: a + b ** 2, v, 0))
+
+    print(f'Solution is {size}')
+
+
+if __name__ == '__main__':
+    main()
