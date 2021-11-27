77dad4838d45ce67c528120fa802de7ebaea4cae
Adrian Szymanowski <adriano100417@gmail.com>
2021-11-27 15:53:02 +0100

    Mathematics - refactor Modular Square Root task with a sage approach

--- Mathematics/modular_square_root.py
+++ Mathematics/modular_square_root.py
@@ -1,81 +1,11 @@
-from math import sqrt
+from sage.all import Mod
+from sage.rings.finite_rings.integer_mod import square_root_mod_prime
 
 
 def main():
     a, p = getInput()
-    result = prime_mod_sqrt(a, p)
-    print(min(result))
-
-
-def prime_mod_sqrt(a, p):
-    """
-    Square root modulo prime number
-    Solve the equation
-        x^2 = a mod p
-    and return list of x solution
-    http://en.wikipedia.org/wiki/Tonelli-Shanks_algorithm
-    """
-    a %= p
-
-    # Simple case
-    if a == 0:
-        return [0]
-    if p == 2:
-        return [a]
-
-    # Check solution existence on odd prime
-    if legendre_symbol(a, p) != 1:
-        return []
-
-    # Simple case
-    if p % 4 == 3:
-        x = pow(a, (p + 2) >> 1, p)
-        return [x, p-x]
-
-    # Factor p-1 on the form q * 2^s (with Q odd)
-    q, s = p - 1, 0
-    while q % 2 == 0:
-        s += 1
-        q //= 2
-
-    # Select a z which is a quadratic non resudue modulo p
-    z = 1
-    while legendre_symbol(z, p) != -1:
-        z += 1
-    c = pow(z, q, p)
-
-    # Search for a solution
-    x = pow(a, (q + 1) // 2, p)
-    t = pow(a, q, p)
-    m = s
-    while t != 1:
-        # Find the lowest i such that t^(2^i) = 1
-        i, t2i, = 0, t
-        for i in range(1, m):
-            t2i = t2i * t2i % p
-            if t2i == 1:
-                break
-
-        # Update next value to iterate
-        b = pow(c, 2**(m - i - 1), p)
-        x = (x * b) % p
-        c = (b * b) % p
-        t = (t * c) % p
-        m = i
-
-    return [x, p-x]
-
-
-def legendre_symbol(a, p):
-    """
-    Legendre symbol
-    Define if a is a quadratic residue modulo odd prime
-    http://en.wikipedia.org/wiki/Legendre_symbol
-    """
-    ls = pow(a, p >> 1, p)
-    if ls == p - 1:
-        return -1
-    return ls
+    result = square_root_mod_prime(Mod(a, p), p)
+    print(f'Solution is {result}')
 
 
 def getInput():
