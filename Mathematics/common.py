231e7d48606168b8e3fe61c8df2e46da509a04db
Adrian Szymanowski <adriano100417@gmail.com>
2021-11-28 17:22:42 +0100

    Mathematics - Find the Lattice

--- Mathematics/common.py
+++ Mathematics/common.py
@@ -1,5 +1,5 @@
 from functools import reduce
-from math import sqrt
+from math import floor, sqrt
 from typing import List
 
 
@@ -27,11 +27,27 @@ def vector_sum(u) -> List[float]:
     return list(reduce(lambda a, b: map(lambda x, y: x + y, a, b), u))
 
 
-# def vector_normalize(u) -> List[float]:
-#     mag = vector_size(u)
-#     return list(map(lambda x: x / mag, u))
+def vector_normalize(u) -> List[float]:
+    mag = vector_size(u)
+    return list(map(lambda x: x / mag, u))
 
 
 def vector_projection(u, v) -> List[float]:
     a = vector_dot(u, v) / vector_magnitude(u)
     return list(map(lambda x: x * a, u))
+
+
+# algorithms
+
+def vector_gaussian(v1, v2):
+    while True:
+        if vector_size(v2) < vector_size(v1):
+            temp = v1
+            v1 = v2
+            v2 = temp
+
+        m = floor(vector_dot(v1, v2) // vector_dot(v1, v1))
+        if m == 0:
+            return v1, v2
+
+        v2 = vector_substract(v2, map(lambda x: m * x, v1))
